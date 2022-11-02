bc18e3c1099aa70140d8c563d8cfb7538cbeb21c
Fabieri <51917750+Fabieri@users.noreply.github.com>
2022-11-02 14:41:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,4 +51,6 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
-
+mycur.execute("SELECT * from fruit_load_list")
+my_data_row = mycur.fetchone()
+streamlit.text(my_data_row)
